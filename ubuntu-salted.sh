a6ec0ca5bc3356c79000da3e65fac00aca75c4c1
Colton Myers <colton.myers@gmail.com>
2013-02-15 17:15:02 -0700

    Add ubuntu-salted script

---
+++ ubuntu-salted.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+exec &> /root/stackscript.log
+
+SSHKEYURL=http://files.basepi.net/ssh_keys/cmyers-alice.pub
+
+mkdir /root/.ssh/
+touch /root/.ssh/authorized_keys
+wget $SSHKEYURL --output-document=/tmp/cmyers-ssh.pub
+cat /tmp/cmyers-ssh.pub >> /root/.ssh/authorized_keys
+
+apt-get update
+apt-get upgrade -y
+echo deb http://ppa.launchpad.net/saltstack/salt/ubuntu `lsb_release -sc` main | tee /etc/apt/sources.list.d/saltstack.list
+wget -q -O- "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x4759FA960E27C0A6" | apt-key add -
+apt-get update
+apt-get install -y salt-minion salt-master salt-syndic
+apt-get install -y zsh python python3 git
+apt-get install -y vim
+
+cd /root
+git clone git://github.com/basepi/dotfiles
+cd dotfiles
+git submodule init
+git submodule update
+./install.py -y
+chsh -s /bin/zsh root
+cd /
